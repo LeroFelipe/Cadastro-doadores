d8f9d784d41f6cf48b11c05cdf3fb624485aa6da
LeroFelipe <lerofelipe@hotmail.com>
2024-03-01 18:01:51 -0300

    teste

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
