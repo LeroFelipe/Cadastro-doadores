895dab024769eaa43047061c945cecacc959d8ca
LeroFelipe <lerofelipe@hotmail.com>
2024-03-07 16:38:58 -0300

    Sistema de autenticação melhorado!

--- api/login.js
+++ api/login.js
@@ -2,18 +2,23 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const users = [
-  { id: 1, username: 'usuario1', password: 'senha123' },
+  { id: 1, usuario: 'sebastiao', password: '01020304' },
   // Adicione mais usuários conforme necessário
 ];
 
 module.exports = async (req, res) => {
-  const { username, password } = req.body;
-  const user = users.find(u => u.username === username && u.password === password);
+  const { usuario, password } = req.body;
+
+  console.log(usuario, password, process.env.JWT_SECRET);
+
+  const user = users.find(u => u.usuario === usuario && u.password === password);
+
+  console.log(user);
 
   if (user) {
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
   } else {
-    res.status(401).json({ message: 'Usuário ou senha incorretos' });
+    res.status(401).json({ message: 'Usuário ou senha incorretos!'});
   }
 };
